53e4399bca462a6e299e95c225e3b647dd2c7012
Karina Trapane <karinatrapane@Karinas-MacBook-Air.local>
2022-03-22 15:30:35 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int money = 1000;
+        System.out.println("Your total: " + money);
+        double percent = money * 0.25;
+        System.out.println("25% of total: " + percent);
+
+
+    }
+}
