d9554fe3dc0f9274e5e1bbfaed032475d19ac0a9
Karina Trapane <karinatrapane@Karinas-MacBook-Air.local>
2022-03-23 15:57:46 +0200

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int money = 1000;
-        System.out.println("Your total: " + money);
-        double percent = money * 0.25;
+	int euros = 1000;
+        System.out.println("Your total: " + euros);
+        double percent = euros * 0.25;
         System.out.println("25% of total: " + percent);
 
 
